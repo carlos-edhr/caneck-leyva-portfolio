4b957115566f3d59b5bf4bfc85b3f220c55404f0
Eduardo Rojas <carlos.edhr@protonmail.com>
2025-04-29 15:27:42 -0700

    fix: added description in artist section of the landing page. working on admin dashboard 2

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -7,30 +7,32 @@ import { Sidebar, SidebarSkeleton } from "./_components/sidebar";
 // import { currentUser } from "@/lib/auth";
 import DashboardFooter from "./_components/dashboard-footer";
 
-interface BrowseLayoutProps {
-  params: { username: string };
-  children: React.ReactNode;
-}
+// interface BrowseLayoutProps {
+//   params: { username: string };
+//   children: React.ReactNode;
+// }
 
-const BrowseLayout = async ({ params, children }: BrowseLayoutProps) => {
-  // try {
-  //   const self = await currentUser();
-  // } catch {
-  //   redirect("/auth/login");
-  // }
+const BrowseLayout = async () =>
+  // { params, children }: BrowseLayoutProps
+  {
+    // try {
+    //   const self = await currentUser();
+    // } catch {
+    //   redirect("/auth/login");
+    // }
 
-  return (
-    <>
-      <Navbar />
-      <div className="flex h-full pt-20 bg-white">
-        {/* <Suspense fallback={<SidebarSkeleton />}>
+    return (
+      <>
+        <Navbar />
+        <div className="flex h-full pt-20 bg-white">
+          {/* <Suspense fallback={<SidebarSkeleton />}>
           <Sidebar />
         </Suspense>
 
         <Container>{children}</Container> */}
-      </div>
-    </>
-  );
-};
+        </div>
+      </>
+    );
+  };
 
 export default BrowseLayout;
